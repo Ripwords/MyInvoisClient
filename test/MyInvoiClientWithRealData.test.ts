3200a8efc2e4f6d96729a617e45c865cc744390c
JJ <teohjjteoh@gmail.com>
2025-04-21 11:02:58 +0800

    test: skip MyInvoisClient tests if required environment variables are missing; use hardcoded values for TIN and NRIC in tests

--- test/MyInvoiClientWithRealData.test.ts
+++ test/MyInvoiClientWithRealData.test.ts
@@ -3,6 +3,18 @@ import { MyInvoisClient } from '../src/utils/MyInvoisClient'
 
 describe('MyInvoisClientWithRealData', () => {
   it('should verify TIN with real data', async () => {
+    // Skip the test if the environment variables are not set
+    if (
+      !process.env.CLIENT_ID ||
+      !process.env.CLIENT_SECRET ||
+      !process.env.TIN_VALUE ||
+      !process.env.NRIC_VALUE
+    ) {
+      expect
+        .soft(false, 'Skipping test: Missing required environment variables')
+        .toBe(true)
+      return
+    }
     const client = new MyInvoisClient(
       process.env.CLIENT_ID!,
       process.env.CLIENT_SECRET!,
